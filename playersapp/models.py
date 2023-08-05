6afe6646a311a767de083bbf94e77ed4be89fcb6
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-05 13:55:43 -0700

    Create model for players
    Each player has a vehicle. Therefore, the Vehicle model is associated with the Player model. Player has-a Vehicle. The vehicle model is in a separate repository. If I recall correctly, this is where Graphene Federation comes into play. Or, perhaps, Node.js is to be utilized here. My memory is a bit hazy. Thus, for the sake of efficiency, the priority now is to simply get Player to display in the administration end of Django. Vehicle model: https://github.com/JoshuaJonesTS/racing-game-vehicles/blob/main/vehiclesapp/models.py

--- playersapp/models.py
+++ playersapp/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
+# The Vehicle model is in a separate repository. If I recall correctly, this is where graphene-federation comes into play. Either that, or Node.js
+# from core.models import VehicleField 
 
 # Create your models here.
+class Player(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=15)
+    # vehicle = models.VehicleField()
+
+    def __str__(self):
+        return self.name
