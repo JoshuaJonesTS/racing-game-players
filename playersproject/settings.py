4007d6503e8419616a254255d69a4ea97944c3fa
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-05 13:39:03 -0700

    Fix oversight

--- playersproject/settings.py
+++ playersproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playersapp.app.PlayersappConfig'
+    'playersapp.apps.PlayersappConfig'
 ]
 
 MIDDLEWARE = [
