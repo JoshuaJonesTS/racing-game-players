30cbb0eb857906f29bb390be93e1a8ec6bfb33b5
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-05 13:37:21 -0700

    Allow all hosts

--- playersproject/settings.py
+++ playersproject/settings.py
@@ -29,7 +29,7 @@ STAGE = os.environ.get('STAGE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
